99602b558aa9e5e63b65d5cb36b70975d5235a69
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-08 12:31:24 -0300

    add diaries route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import diaryRouter from './routes/diaries'
 
 const app = express()
 app.use(express.json())
@@ -9,6 +10,8 @@ app.get('/ping', (_req, res)=>{
     console.log('Some is pinged here UWU')
     res.send('ping')
 })
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
     console.log('Server is running on port 3000')
