46989b7056fb192ba56f6b4debd02a7692116223
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-08 15:30:05 -0300

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ app.use(express.json())
 
 const PORT = 3000
 
-app.get('/ping', (_req, res)=>{
-    console.log('Some is pinged here UWU')
-    res.send('ping')
+app.get('/ping', (_req, res) => {
+  console.log('Some is pinged here UWU')
+  res.send('ping')
 })
 
 app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000')
+  console.log('Server is running on port 3000')
 })
