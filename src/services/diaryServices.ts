e127c78a4318bd853474460697070473ce19bffa
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-08 14:14:54 -0300

    add  new services

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,8 +1,37 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, NewDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
-export const addEntry = (): undefined => undefined
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = diaries.find((diary) => diary.id === id)
+  if (entry != null) {
+    const { comment, ...restOfDiary } = entry
+    return restOfDiary
+  }
+  return undefined
+}
+
+// data without comment
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id,
+      date,
+      weather,
+      visibility
+    }
+  })
+}
+
+export const addDiary = (newDiaryEntry: NewDiaryEntry): DiaryEntry => {
+  const newDiary = {
+    id: Math.max(...diaries.map(diary => diary.id)) + 1,
+    ...newDiaryEntry
+  }
+
+  diaries.push(newDiary)
+  return newDiary
+}
